1de743987c251e55687292c1b480acb626fdca91
Nikolay Makalov <nexterrible@mail.ru>
2022-04-25 01:14:05 +0700

    Тестовый консольный проект

---
+++ Tests/WPF_learningConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPF_learningConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
